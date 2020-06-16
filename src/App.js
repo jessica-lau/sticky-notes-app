4a5afa96ed52b7f8900e5411481465d0be86150a
jessica-lau <jessica.lau2@gmail.com>
2020-06-16 17:31:50 -0400

    add class names to JSX to apply css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
 
+
 var App = () => (
     <div>
-        <header>
-            <h1>Super Sticky Notes</h1>
-            <aside>
-                <button>+ New Note</button>
-                <input type="text" placeholder="Type here to search..." />
+        <header class= "app-header">
+            <h1 class="app-header__title">Super Sticky Notes</h1>
+            <aside class= "app-header__controls">
+                <button class="add-new">+ New Note</button>
+                <input class="search" type="text" placeholder="Type here to search..." />
             </aside>
         </header>
-        <ul>
-            <li>
-                <input type="text" placeholder="Title" />
-                <textarea placeholder="Description..." />
-                <span>X</span>
+        <ul class= "notes-list">
+            <li class= "note">
+                <input class= "note__title" type="text" placeholder="Title" />
+                <textarea class= "note__description" placeholder="Description..." />
+                <span class= "note__delete">X</span>
             </li>
-            <li>
-                <input type="text" placeholder="Title" />
-                <textarea placeholder="Description..." />
-                <span>X</span>
+            <li class= "note">
+                <input class= "note__title" type="text" placeholder="Title" />
+                <textarea class= "note__description" placeholder="Description..." />
+                <span class= "note__delete">X</span>
             </li>
-            <li>
-                <input type="text" placeholder="Title" />
-                <textarea placeholder="Description..." />
-                <span>X</span>
+            <li class= "note">
+                <input class= "note__title" type="text" placeholder="Title" />
+                <textarea class= "note__description" placeholder="Description..." />
+                <span class= "note__delete">X</span>
             </li>
         </ul>
     </div>
