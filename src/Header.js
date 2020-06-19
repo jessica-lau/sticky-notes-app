6ba50844e7e3f65481ff1b5e7e0761e47dd53e78
jessica-lau <jessica.lau2@gmail.com>
2020-06-18 22:35:55 -0400

    create stateful components and props

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-var Header = () => (
+var Header = props => (
     <header class="app-header">
         <h1 class="app-header__title">Super Sticky Notes</h1>
         <aside class="app-header__controls">
             <button class="add-new">+ New Note</button>
-            <input class="search" type="text" placeholder="Type here to search..." />
+            <input value={props.searchText} class="search" type="text" placeholder="Type here to search..." />
         </aside>
     </header>
 );
--- src/Note.js
+++ src/Note.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-var Note = () => (
+var Note = props => (
     <li class="note">
-        <input class="note__title" type="text" placeholder="Title" />
-        <textarea class="note__description" placeholder="Description..." />
+        <input value={props.notes.title} class="note__title" type="text" placeholder="Title" />
+        <textarea value={props.notes.description} class="note__description" placeholder="Description..." />
         <span class="note__delete">X</span>
     </li>
 );
