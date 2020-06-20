d66e713906b0e185a69e1f0ed000e8c4ccba244a
jessica-lau <jessica.lau2@gmail.com>
2020-06-19 21:39:15 -0400

    add props to notesList module

--- src/Note.js
+++ src/Note.js
@@ -2,8 +2,8 @@ import React from "react";
 
 var Note = props => (
     <li class="note">
-        <input value={props.notes.title} class="note__title" type="text" placeholder="Title" />
-        <textarea value={props.notes.description} class="note__description" placeholder="Description..." />
+        <input value={props.note.title} class="note__title" type="text" placeholder="Title" />
+        <textarea value={props.note.description} class="note__description" placeholder="Description..." />
         <span class="note__delete">X</span>
     </li>
 );
--- src/NotesList.js
+++ src/NotesList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Note from "./Note.js";
 
-var NotesList = () => (
-            <ul class="notes-list">
-                <Note />
-                <Note />
-                <Note />
-            </ul>
-);
+var keepMatchSearch = note => note.doesMatchSearch;
+var renderNote = note => <Note note={note} key={note.id} />
+
+var NotesList = props => {
+    var searchMatches = props.notes.filter(keepMatchSearch);
+    var notesElements = searchMatches.map(renderNote);
+    return <ul class="notes-list">{notesElements}</ul>;
+
+}
 
 export default NotesList;
