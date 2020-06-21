164e695ad2566ed46396b323bbe0f376909cb050
jessica-lau <jessica.lau2@gmail.com>
2020-06-21 14:14:55 -0400

    add search method

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ var Header = props => (
         <h1 class="app-header__title">Super Sticky Notes</h1>
         <aside class="app-header__controls">
             <button class="add-new" onClick={props.addNew} >+ New Note</button>
-            <input value={props.searchText} class="search" type="text" placeholder="Type here to search..." />
+            <input value={props.searchText} class="search" type="text" placeholder="Type here to search..." onChange={props.onSearch} />
         </aside>
     </header>
 );
